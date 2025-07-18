14ac1521c5f6e2276f1392c5463e292d0ae1d009
Daniel Waiguru <danielirungu91@gmail.com>
2025-07-18 17:49:37 +0300

    refactor: introduce jvm library convention plugin
    This commit introduces a new convention plugin `quickstay.jvm.library` to standardize the configuration for JVM library modules.
    
    The `features/booking/contract/build.gradle.kts` file has been updated to use this new convention plugin, removing the explicit configuration for Java and Kotlin compilation.

--- features/booking/contract/build.gradle.kts
+++ features/booking/contract/build.gradle.kts
@@ -1,13 +1,3 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.org.jetbrains.kotlin.jvm)
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+    id("quickstay.jvm.library")
 }
