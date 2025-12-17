868f8be883e06365c11f9cb650b2b5b03b66c3aa
Daniel Waiguru <danielirungu91@gmail.com>
2025-12-17 06:58:49 +0300

    feat(deps): update hilt navigation compose to lifecycle viewmodel compose

--- features/booking/lib/src/main/java/com/danielwaiguru/tripitacaandroid/booking/lib/BookingScreen.kt
+++ features/booking/lib/src/main/java/com/danielwaiguru/tripitacaandroid/booking/lib/BookingScreen.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.danielwaiguru.tripicaandroid.designsystem.components.TripitacaPrimaryButton
 import com.danielwaiguru.tripicaandroid.designsystem.components.TripitacaRoundedInputField
